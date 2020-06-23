fa7639ec8122989daa51481b420348800226251e
Markus Tacker <Markus.Tacker@NordicSemi.no>
2020-06-23 20:51:36 +0200

    feat: initial configuration

---
+++ README.md
@@ -0,0 +1,4 @@
+# [Sanity.io](https://sanity.io) Studio Configuration for [adacon.no](https://github.com/tech-women/adacon.git)
+
+    npm ci
+    npm start
---
+++ schemas/schema.js
@@ -0,0 +1,65 @@
+import createSchema from "part:@sanity/base/schema-creator";
+import schemaTypes from "all:part:@sanity/base/schema-type";
+
+export default createSchema({
+  name: "adacon20",
+  types: schemaTypes.concat([
+    {
+      name: "gallery",
+      type: "document",
+      title: "Gallery",
+      fields: [
+        {
+          name: "images",
+          type: "array",
+          options: {
+            layout: "grid",
+          },
+          of: [
+            {
+              type: "image",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      name: "speaker",
+      type: "document",
+      title: "Speaker",
+      fields: [
+        {
+          name: "images",
+          type: "array",
+          options: {
+            layout: "grid",
+          },
+          of: [
+            {
+              type: "image",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      name: "venue",
+      type: "document",
+      title: "Venue",
+      fields: [
+        {
+          name: "images",
+          type: "array",
+          options: {
+            layout: "grid",
+          },
+          of: [
+            {
+              type: "image",
+            },
+          ],
+        },
+      ],
+    },
+  ]),
+});
